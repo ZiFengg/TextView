bf9244b7816b696e80f4a2855e977f535e608063
ZiFeng <1174335928@qq.com>
2018-03-24 19:51:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zifeng/textview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.zifeng.textview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZiFeng on 2018/3/17.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
